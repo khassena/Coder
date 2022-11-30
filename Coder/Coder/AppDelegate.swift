f38102afa3b12cdf06c6040660e49fd3259adb88
Amanzhan Khassen <zharkynuly11@gmail.com>
2022-11-30 18:23:04 +0600

    Created project Coder

---
+++ Coder/Coder/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Coder
+//
+//  Created by Amanzhan Zharkynuly on 30.11.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ Coder/Coder/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Coder
+//
+//  Created by Amanzhan Zharkynuly on 30.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
